464ffa8ad48506c7a9deb2d6114216b1530361c7
islam.draz <eng.islamdraz@gmail.com>
2020-05-31 15:17:35 +0200

    add child routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -7,26 +7,44 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { ProductResolver } from './product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     SharedModule,
+    FormsModule,
     RouterModule.forChild([
-      {path:'products',component:ProductListComponent},
-      {
-        path:'products/:id',component:ProductDetailComponent,
-        resolve:{productResolved:ProductResolver}
-      },
-      {
-        path:'products/:id/edit',component:ProductEditComponent,
-        resolve:{productResolved:ProductResolver}
-      },
+      {path:'products',
+      children:[
+        {path:'',component:ProductListComponent},
+        {
+          path:':id',component:ProductDetailComponent,
+          resolve:{productResolved:ProductResolver}
+        },
+        {
+          path:':id/edit',component:ProductEditComponent,
+          resolve:{productResolved:ProductResolver},
+          children:[
+            {path:'',redirectTo:'info',pathMatch:'full'},
+            {path:'info',component:ProductEditInfoComponent},
+            {path:'tags',component:ProductEditTagsComponent},
+          ]
+          
+        }
+      ]
+      
+    },
+      
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
